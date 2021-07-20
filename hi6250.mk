bf147455bac658562a07aede8c6057cc0c2391b4
Meticulus <theonejohnnyd@gmail.com>
2021-07-20 15:33:06 +0200

    hi6250: import oss hwcomposer module
    Change-Id: Ie8ee37dbd013b90b43e959b681dfcdde1b422e67

--- hi6250.mk
+++ hi6250.mk
@@ -89,6 +89,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.renderscript@1.0-impl \
+    hwcomposer.hi6250 \
     libion
 
 # Health
